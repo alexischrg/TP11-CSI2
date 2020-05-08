c4dac8426dfb0ba1bf066b5e35ae8ed7d7e584cf
alexischrg <alexis.chargy@isen-ouest.yncrea.fr>
2020-05-08 19:53:06 +0200

    version comm

--- exercice 4.py
+++ exercice 4.py
@@ -20,12 +20,17 @@ class Duree:
         return Duree(h,m,s)
 
     def __str__(self):
-        return (str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s")
+        return (str(self.__heure) + "h " + str(self.__minute) + "m " + str(self.__seconde) + "s ")
+
+    """Digital Version
+    def __str__(self):
+        return (str(self.__heure) + ":" + str(self.__minute) + ":" + str(self.__seconde))
+        """
 
 
 if __name__ == '__main__':
     dur1 = Duree(13,15,50)
     dur2 = Duree(1,50, 12)
     durtot = dur1 + dur2
-    print(dur1)
-    print(durtot)
+    print(dur1) #13h 15m 50s
+    print(durtot) #50+12=62s=1m2s et 50+15+1=66m=1h6m et 13+1+1=15 DONC 15h 6m 2s
